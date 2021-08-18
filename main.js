7ef9e3a91efcdf3fb003a23fa0121c927350d130
Praneet0212 <71916363+Praneet0212@users.noreply.github.com>
2021-08-18 19:16:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -34,6 +34,37 @@ function draw()
 
     fill("#FF0000");
     stroke("#FF0000");
+
+    circle(rightWristX, rightWristY, 20);
+
+    if(scoreRightWrist > 0.2)
+    {
+        if(rightWristY > 0 && rightWristY <= 100)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 0.5x";
+            song.rate(0.5);
+        }
+        else if(rightWristY > 100 && rightWristY <= 200)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 1x";
+            song.rate(1);
+        }
+        else if(rightWristY > 200 && rightWristY <= 300)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 1.5x";
+            song.rate(1.5);
+        }
+        else if(rightWristY > 300 && rightWristY <= 400)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 2x";
+            song.rate(2);
+        }
+        else if(rightWristY > 400 && rightWristY <= 500)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 2.5x";
+            song.rate(2.5);
+        }
+    }
     
     if(scoreLeftWrist > 0.2)
     {
@@ -56,6 +87,10 @@ function play()
 function stop()
 {
     song.stop();
+    song.setVolume(1);
+    song.rate(1);
+    document.getElementById("speed").innerHTML = "Speed";
+    document.getElementById("volume").innerHTML = "Volume";
 }
 
 function gotPoses(results)
@@ -63,8 +98,9 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
         scoreLeftWrist = results[0].pose.keypoints[9].score;
-        console.log("scoreLeftWrist = " + scoreLeftWrist);
+        console.log("scoreLeftWrist = " + scoreLeftWrist + " scoreRightWrist = " + scoreRightWrist);
 
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
